4570e84eb5b3bce6a0ccd1b1fbf30cd8506b8b29
divyang9414 <divyang.rnw@gmail.com>
2024-05-03 19:35:53 +0530

    Some Changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from './components/header/Header';
 import Topbar from './components/topbar/Topbar';
 import Home from './pages/home/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
   <>
-    <Header />
     <Topbar />
+    <Header />
     <Home/>
+    {/* <Navbar/> */}
   </>
   );
 }
